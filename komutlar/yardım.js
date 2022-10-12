499edbd6b7ff025282fa0e159b4dd4c99b3368b5
UnknownDeveloptment <115506384+UnknownDeveloptment@users.noreply.github.com>
2022-10-12 23:47:26 +0300

    Create yardım.js

---
+++ komutlar/yardım.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name:"yardım",
+alliases:['y'],
+  code:`$description[
+  $color[RANDOM]
+!çal -> !çal {Şarkı adı}
+!durdur
+!devam
+!tekrarla
+!geç
+!çık
+!sıralama
+!ses -> (1-100)
+!ping - !p
+]`
+}
+//"!" işareti yerine prefixinizi yazın.
